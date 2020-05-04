6336ef0e4e60da77191956c2bbdb3e79e8d81673
Julien Charton <jul.charton@gmail.com>
2020-05-04 18:18:19 +1000

    test3

--- curl_test.sh
+++ curl_test.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -l
 
-echo "Initiating Test Type:  $TYPE_OF_TEST"
-echo "Results for $TYPE_OF_TEST to the URL $URL:"
-CURL_RESPONSE=$(curl -v $URL)
-echo "\n\n\n The CURL Response is below:"
-echo "$CURL_RESPONSE"
+echo "Initiating Test Type:"
+# curl --location --request POST 'https://api.codemagic.io/builds' \
+# --header 'Authorization: Bearer vID_NgJVFMgIrH4FtE78RwhIBjokX6OZUHF95vy_KWU' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "appId": "5e7d6b5718c126107f65019e",
+#     "workflowId": "5e7d6b5718c126107f65019d-",
+#     "branch": "master"
+# }'
